dc0cdc2ee7cccdb52ab3492d488b3d8067a3a4c6
Reece Carolan <reece@hellostudios.io>
2020-10-09 00:03:13 -0700

    Greatly improved image sizing

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import Project from "../models/Project";
+import Container from "@material-ui/core/Container";
 
 type Props = {
   project: Project;
@@ -14,10 +15,11 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     outer: {
       height: "100%",
+      padding: theme.spacing(2),
       // display: "flex"
     },
     textBlock: {
-      maxWidth: 655,
+      maxWidth: 600,
     },
     textStyle: {
       color: (props: Props) => props.project.titleColor,
@@ -28,13 +30,23 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     image: {
       objectFit: "cover",
+      // alignContent: "center",
       width: "100%",
+      maxWidth: 600,
+      display: "flex",
+      // alignSelf: "flex-end",
+    },
+
+    imageBox: {
       alignSelf: "flex-end",
+      // textAlign: "center",
     },
     gridContent: {
+      // minHeight: 400,
       display: "flex",
       flexGrow: 1,
       flexShrink: 1,
+      marginTop: theme.spacing(3),
     },
   })
 );
@@ -46,29 +58,38 @@ export default function ProjectTile(props: Props) {
     //Horizantal container grid for card
     <Grid item xs={12} sm={props.project.isWide ? 12 : 6}>
       <Card variant="outlined" className={classes.outer}>
+        {/* <Box m={4}> */}
         {/* Vertical grid for card contents */}
         <Grid container direction="column" className={classes.outer}>
           <Grid item>
-            <Box m={4}>
-              <Typography variant="h3" className={classes.textStyle}>
-                {props.project.title}
-              </Typography>
-              <Typography variant="body1" className={classes.textBlock}>
-                {props.project.description}
-              </Typography>
-              <Typography variant="body2" className={classes.subtitle}>
-                {props.project.subtitleDescription}
-              </Typography>
-            </Box>
+            {/* <Box m={4}> */}
+            <Typography variant="h3" className={classes.textStyle}>
+              {props.project.title}
+            </Typography>
+            <Typography variant="body1" className={classes.textBlock}>
+              {props.project.description}
+            </Typography>
+            <Typography variant="body2" className={classes.subtitle}>
+              {props.project.subtitleDescription}
+            </Typography>
+            {/* </Box> */}
           </Grid>
           <Grid item className={classes.gridContent}>
-            <img
-              src={props.project.image}
-              alt="Project Image"
-              className={classes.image}
-            />
+            <Box
+              justifyContent="center"
+              display="flex"
+              width="100%"
+              className={classes.imageBox}
+            >
+              <img
+                src={props.project.image}
+                alt="Project Image"
+                className={classes.image}
+              />
+            </Box>
           </Grid>
         </Grid>
+        {/* </Box> */}
       </Card>
     </Grid>
   );
--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -3,7 +3,7 @@ import Project from "../models/Project";
 export const projectData: Project[] = [
   {
     title: "HelloCampus",
-    image: "/assets/HelloCampusHero.png",
+    image: "/assets/HelloCampus.png",
     description:
       "This is my biggest project to date. HelloCampus helps students share and discover events across their college campus. One of its core features is a beautiful native iOS app written in Swift.",
     subtitleDescription:
@@ -30,13 +30,22 @@ export const projectData: Project[] = [
     titleColor: "#27ae60",
   },
   {
+    title: "HelloCRM",
+    image: "/assets/HelloCRM.png",
+    description:
+      "A modern CRM that runs right on your phone. It takes full advantage of the Apple ecosystem to make managing your network a breeze.",
+    subtitleDescription: "Swift 5, Google Cloud Platform, Stripe",
+    isWide: true,
+    titleColor: "#0064E9",
+  },
+  {
     title: "HelloStudios LLC",
     image: "/assets/HelloStudios.png",
     description:
       "I co-founded HelloStudios to create and maintain eco-friendly websites. We developed and hosted 18 websites, reaching over two million people.",
     subtitleDescription: "Wordpress, Cloudflare, Graphic Design",
     isWide: true,
-    titleColor: "#8e44ad",
+    titleColor: "#0064E9",
   },
   {
     title: "Portfolio Site",
@@ -54,6 +63,6 @@ export const projectData: Project[] = [
       "I developed a native iOS app which communicated with AWS to generate low-cost meal plans over any given week. Ingredient prices and availability was collected by web scraping.",
     subtitleDescription: "Swift 5, AWS (Lambda, Aurora), Public Speaking",
     isWide: false,
-    titleColor: "#0064E9",
+    titleColor: "#a02133",
   },
 ];
