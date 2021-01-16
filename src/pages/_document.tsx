fd64bef20ccdec3e239b1e752e825306ce827cfc
Reece Carolan <reece@hellostudios.io>
2021-01-16 04:15:17 -0800

    Updated deps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import createEmotionServer from "@emotion/server/create-instance";
+// import createEmotionServer from "@emotion/server/create-instance";
 // import theme from "../theme";
 // import { cache } from "./_app";
 
