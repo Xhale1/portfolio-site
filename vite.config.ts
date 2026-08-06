06590f1ff884b7ac084227149083e5c6f17603bd
Reece Carolan <reececarolan@gmail.com>
2026-08-06 02:30:20 -0700

    chore: clean vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [tanstackStart(), nitro(), viteReact()],
 });
