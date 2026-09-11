fdb239a246e46b688aa5a9ea0eaa164cc19b1f76
Reece Carolan <reececarolan@gmail.com>
2026-09-10 20:55:43 -0700

    chore: bump deps

--- src/router.tsx
+++ src/router.tsx
@@ -5,8 +5,6 @@ export function getRouter() {
   const router = createRouter({
     routeTree,
     defaultPreload: "intent",
-    defaultErrorComponent: (err) => <p>{err.error.stack}</p>,
-    defaultNotFoundComponent: () => <p>not found</p>,
     scrollRestoration: true,
   });
 
